43db131eed3a816d933e6d7df85e26a118e75086
Sascha Brawer <sascha@brawer.ch>
2024-03-21 17:43:03 +0100

    Improve splitting

--- src/split.py
+++ src/split.py
@@ -68,8 +68,18 @@ def read_street_abbrevs(streets):
     return result
 
 
+def read_pois():
+    result = set()
+    filepath = os.path.join(os.path.dirname(__file__), "pois.csv")
+    with open(filepath) as stream:
+        for row in csv.DictReader(stream):
+            result.add(row["PointOfInterest"])
+    return result
+
+
 TITLES = read_titles()
 OCCUPATIONS = read_occupations()
+POIS = read_pois()
 STREETS = read_streets()
 STREET_ABBREVS = read_street_abbrevs(STREETS)
 
@@ -78,6 +88,8 @@ def is_valid_address(addr):
     if m := re.match(r"^(.+) (\d+[a-t]?)$", addr):
         street, num = m.groups()
         return (street in STREET_ABBREVS) or (street in STREETS)
+    if addr in POIS:
+        return True
     return False
 
 
@@ -98,8 +110,8 @@ def split(vol):
         line = line.strip()
         if m := page_re.match(line):
             date, page, page_num = m.groups()
-            sheet = create_sheet(workbook, page_num)
-            row = 1
+            sheet = create_sheet(workbook, page, page_num)
+            row = 2
             page = f"https://www.e-rara.ch/bes_1/periodical/pageview/{page}"
             continue
         p, pos = line.split("#", 1)
@@ -138,14 +150,20 @@ def split(vol):
             and other_ok
         )
 
-        # scan
+        # id
         cell = sheet.cell(row, 1)
+        cell.value, cell.font = pos, font
+        if not all_ok:
+            cell.fill = light_red_fill
+
+        # scan
+        cell = sheet.cell(row, 2)
         cell.value, cell.font = "", font
         if not all_ok:
             cell.fill = light_red_fill
 
         # family name
-        cell = sheet.cell(row, 2)
+        cell = sheet.cell(row, 3)
         cell.value, cell.font = name, font
         if not family_name_ok:
             cell.fill = red_fill
@@ -153,7 +171,7 @@ def split(vol):
             cell.fill = light_red_fill
 
         # given name
-        cell = sheet.cell(row, 3)
+        cell = sheet.cell(row, 4)
         cell.value, cell.font = givenname, font
         if not given_name_ok:
             cell.fill = red_fill
@@ -161,7 +179,7 @@ def split(vol):
             cell.fill = light_red_fill
 
         # maiden name
-        cell = sheet.cell(row, 4)
+        cell = sheet.cell(row, 5)
         cell.value, cell.font = maidenname, font
         if not maiden_name_ok:
             cell.fill = red_fill
@@ -169,7 +187,7 @@ def split(vol):
             cell.fill = light_red_fill
 
         # title
-        cell = sheet.cell(row, 5)
+        cell = sheet.cell(row, 6)
         cell.value, cell.font = title, font
         if not title_ok:
             cell.fill = red_fill
@@ -177,7 +195,7 @@ def split(vol):
             cell.fill = light_red_fill
 
         # occupation
-        cell = sheet.cell(row, 6)
+        cell = sheet.cell(row, 7)
         cell.value, cell.font = occupation, font
         if not occupation_ok:
             cell.fill = red_fill
@@ -185,7 +203,7 @@ def split(vol):
             cell.fill = light_red_fill
 
         # address
-        cell = sheet.cell(row, 7)
+        cell = sheet.cell(row, 8)
         cell.value, cell.font = address, font
         if not address_ok:
             cell.fill = red_fill
@@ -193,7 +211,7 @@ def split(vol):
             cell.fill = light_red_fill
 
         # address2
-        cell = sheet.cell(row, 8)
+        cell = sheet.cell(row, 9)
         cell.value, cell.font = address2, font
         if not address2_ok:
             cell.fill = red_fill
@@ -201,7 +219,7 @@ def split(vol):
             cell.fill = light_red_fill
 
         # other
-        cell = sheet.cell(row, 9)
+        cell = sheet.cell(row, 10)
         cell.value, cell.font = other, font
         if not other_ok:
             cell.fill = red_fill
@@ -234,6 +252,8 @@ def split(vol):
 def split_familyname(n):
     n = n.replace(" - ", "-")
     words = n.split()
+    if words[0] in {"de", "De"}:
+        return ("de " + words[1], " ".join(words[2:]))
     if words[0] in {"v.", "V.", "von", "Von"}:
         if words[1].endswith(
             "-v."
@@ -277,6 +297,10 @@ def split_title(p):
 def split_address(p):
     if len(p) == 0:
         return ("", "", [])
+    if is_valid_address(p[0]):
+        return (p[0], "", p[1:])
+    if is_valid_address(p[-1]):
+        return (p[-1], "", p[0:-1])
     last = p[-1].removesuffix(".")
     if m := re.match(r"(.+\d+) ([a-t])", last):
         addr, rest = ("".join(m.groups()), p[:-1])
@@ -309,11 +333,19 @@ def list_volumes():
     )
 
 
-def create_sheet(workbook, page_num):
+def create_sheet(workbook, page_id, page_num):
     sheet = workbook.create_sheet(page_num)
     font = openpyxl.styles.Font(name="Calibri", bold=True)
+    gray = openpyxl.styles.colors.Color(rgb="00DDDDDD")
+    gray_fill = openpyxl.styles.fills.PatternFill(patternType="solid", fgColor=gray)
+    cell = sheet.cell(1, 1)
+    cell.value = f"https://www.e-rara.ch/bes_1/periodical/pageview/{page_id}"
+    cell.font = font
+    cell.fill = gray_fill
+    sheet.merge_cells("A1:J1")
     for i, col in enumerate(
         [
+            "ID",
             "Scan",
             "Name",
             "Vorname",
@@ -325,9 +357,11 @@ def create_sheet(workbook, page_num):
             "Unklar",
         ]
     ):
-        cell = sheet.cell(1, i + 1)
+        cell = sheet.cell(2, i + 1)
         cell.value = col
         cell.font = font
+        cell.fill = gray_fill
+    sheet.column_dimensions["A"].width = 3
     return sheet
 
 
