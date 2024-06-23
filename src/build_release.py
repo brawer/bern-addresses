ea28044ca86a3f446b0f5dacaadc1c98bb975610
Sascha Brawer <sascha@brawer.ch>
2024-06-23 11:07:35 +0200

    Validate occupations in final release data

--- src/build_release.py
+++ src/build_release.py
@@ -15,6 +15,7 @@ class Validator:
         self.family_names = self.read_lines("family_names.txt")
         self.given_names = self.read_lines("givennames.txt")
         self.nobility_names = self.read_csv("nobility_names.csv", "Abkürzung")
+        self.occupations = self.read_csv("occupations.csv", "Occupation")
         self.missing_family_names = set()
         self.re_von = re.compile(r"\b([vV]\.)")  # "Bondeli-v. Allmen"
 
@@ -29,27 +30,35 @@ class Validator:
                 print(name)
 
     def validate(self, entry, pos):
-        self.validate_given_name(entry, pos)
+        bad = set()
+        if not self.validate_given_name(entry, pos):
+            bad.add("Vorname")
         is_company = entry["Titel"] == "[Firma]"
         if is_company:
-            return self.validate_company(entry, pos)
+            bad.update(self.validate_company(entry, pos))
+            return bad
         if name := entry["Adelsname"]:
             if name not in self.nobility_names:
                 self.warn('unknown nobility name "%s"' % name, entry, pos)
+                bad.add("Adelsname")
         for p in ("Name", "Ledigname"):
-            if is_company and p == "Name":
-                continue
             if name := entry[p]:
                 name = self.re_von.sub("von", name)
                 if name not in self.family_names:
                     self.missing_family_names.add(name)
                     message = '%s "%s" not a known family name' % (p, name)
                     self.warn(message, entry, pos)
+                    bad.add(p)
+        bad.update(self.validate_occupations(entry, pos))
+        return bad
 
     def validate_company(self, entry, pos):
+        bad = set()
         for p in ("Adelsname", "Ledigname"):
             if entry[p]:
                 self.warn("%s should not be set on companies" % p, entry, pos)
+                bad.add(p)
+        return bad
 
     def validate_given_name(self, entry, pos):
         given_names = entry["Vorname"].split()
@@ -58,6 +67,15 @@ class Validator:
             message = 'unknwn given name "%s"' % entry["Vorname"]
             self.warn(message, entry, pos)
         return ok
+
+    def validate_occupations(self, entry, pos):
+        bad = set()
+        for p in ("Beruf", "Beruf 2"):
+            if occ := entry[p]:
+                if occ not in self.occupations:
+                    self.warn('unknown occupation "%s"' % occ, entry, pos)
+                    bad.add(p)
+        return bad
 
     def read_lines(self, filename):
         path = os.path.join(os.path.dirname(__file__), filename)
@@ -71,7 +89,7 @@ class Validator:
         result = {}
         with open(path, mode="r") as stream:
             for row in csv.DictReader(stream):
-                assert row[key] not in result, "duplicate entry: %s" % row[0]
+                assert row[key] not in result, "duplicate entry: %s" % row[key]
                 result[row[key]] = row
         return result
 
