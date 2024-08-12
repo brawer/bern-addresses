cace20837d25b31c3be0ca0964f3b13da44c7270
A <randomao@nuvisions.ch>
2024-08-12 18:11:43 +0200

    fix fix_line_order.py: blackhole invalid entries across all volumes
    Then rerun fix_line_order.py across full set.
    
    Also introduces whitelist to change gating behavior in blackhole_lines.py.

--- src/cleanup/blackhole_lines.py
+++ src/cleanup/blackhole_lines.py
@@ -17,9 +17,29 @@ def strip_lines():
        lut[line] = 0
   print("Loaded %s lines to blackhole." % len(lut))
 
+  whitelist = [
+    '1861-04-15.txt',
+    '1881-09-30.txt',
+    '1904-11-30.txt',
+    '1918-01-31.txt',
+    '1918-12-15.txt',
+    '1926-12-15.txt',
+    '1927-12-15.txt',
+    '1928-12-15.txt',
+    '1930-12-15.txt',
+    '1931-12-15.txt',
+    '1932-12-15.txt',
+    '1933-12-15.txt',
+    '1937-12-05.txt',
+    '1938-12-15.txt',
+    '1939-12-31.txt',
+    '1940-12-15.txt',
+    '1941-12-15.txt',
+    '1943-12-15.txt',
+  ]
   for vol in list_volumes():
     # TODO(otz): remove gate
-    if os.path.split(vol)[1] != '1881-09-30.txt':
+    if os.path.split(vol)[1] not in whitelist:
       continue
     print("Blackholing lines in %s" % vol)
 
