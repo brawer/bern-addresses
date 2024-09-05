be80fa04086ec9f1197fe07e45e0195316cbb8e9
A <133387795+random-ao@users.noreply.github.com>
2024-09-05 14:24:00 +0200

    Make fix_indentation slightly more nuanced. (#143)

--- src/cleanup/fix_indentation.py
+++ src/cleanup/fix_indentation.py
@@ -14,19 +14,35 @@ def list_volumes():
         [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".txt")]
     )
 
+def trim(s):
+    return s.strip().replace(',', '')
 
 def fix_indentation():
     gnpath = os.path.join(os.path.dirname(__file__), 'frequent_given_names.txt')
     # TODO(random-ao): generate a freq list, family_names.txt/.. are too broad
     lnpath = os.path.join(os.path.dirname(__file__), 'frequent_last_names.txt')
+    occpath = os.path.join(os.path.dirname(__file__), '..', 'occupations.csv')
     givennames = {name.strip() for name in open(gnpath, 'r')}
     lastnames = {name.strip() for name in open(lnpath, 'r')}
+    occupations = {occ.split(',')[0] for occ in open(occpath, 'r')}
+
     for vol in list_volumes():
         print('Processing indentation fixes for %s' % vol)
         with open(vol + '.tmp', 'w') as out:
+
+            # store first 3 chars of last line
+            # so we can look around
+            last_prefix = ''
+
             for line in open(vol, 'r'):
                 line = line.strip()
                 if not line:
+                    continue
+
+                # skip if indented already
+                if line[0] == '-':
+                    last_prefix = line[:3]
+                    out.write(line + '\n')
                     continue
 
                 # split by whitespace (original approach)
@@ -37,28 +53,62 @@ def fix_indentation():
                 # check if needed, also strips too much,..
 
                 ws_splits = line.split()
-                if (ws_splits[0].strip() in lastnames
-                    and ws_splits[1].strip() in givennames):
+                if (trim(ws_splits[0]) in lastnames
+                    and trim(ws_splits[1]) in givennames):
+
+                        # if the previous line started with
+                        # the same char, this might be a
+                        # lastname
+                        if last_prefix[0] == trim(ws_splits[0])[0]:
+                            out.write(line + '\n')
+                            last_prefix = line[:3]
+                            continue
+
+                        # if last line was indented and the
+                        # same char, it might be a givenname
+                        if last_prefix[2] == trim(ws_splits[0])[0]:
+                            line_out = '- %s\n' % line
+                            out.write(line_out)
+                            last_prefix = line_out[:3]
+                            continue
+
+                        # if the next segment is a known
+                        # occupation, it might be a givenname
+                        if line.split(',')[1].strip() in occupations:
+                            line_out = '- %s\n' % line
+                            out.write(line_out)
+                            last_prefix = line_out[:3]
+                            continue
+
+                        last_prefix = line[:3]
                         out.write(line + '\n')
                         continue
 
+                # TODO(random-ao): refactor, do the same as for ws_splits
                 comma_splits = line.split(',')
-                if (comma_splits[0].strip() in lastnames
-                    and comma_splits[1].strip() in givennames):
+                if (trim(comma_splits[0]) in lastnames
+                    and trim(comma_splits[1]) in givennames):
+                        if last_prefix[2] == trim(ws_splits[0])[0]:
+                            out.write('- ' + line + '\n')
+                            last_prefix = '- ' + line[0]
+                            continue
+                        last_prefix = line[:3]
                         out.write(line + '\n')
                         continue
 
-                # TODO(random-ao): add two more checks
-                # 1. if 1st is lastname and 2nd is job
-                # 2. if 1st is lastname and 2nd is street
+                # TODO(random-ao): add one more check
+                # - if 1st is lastname and 2nd is street
 
                 # check if first segment is known givenname
-                if (ws_splits[0].strip() in givennames
-                    or comma_splits[0].strip() in givennames):
-                        out.write('- ' + line + '\n')
+                if (trim(ws_splits[0]) in givennames
+                    or trim(comma_splits[0]) in givennames):
+                        line_out = '- %s\n' % line
+                        out.write(line_out)
+                        last_prefix = line_out[:3]
                         continue
 
                 # no match found
+                last_prefix = line[:3]
                 out.write(line + '\n')
         os.rename(vol + '.tmp', vol)
 
