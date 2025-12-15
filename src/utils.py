449831143e630e92d85b486a9b7e986d17b4381d
Sascha Brawer <sascha@brawer.ch>
2025-12-15 15:54:57 +0100

    Support command-line options for layout analysis
    For easier debugging with specific pages or entire volumes.
    
    https://github.com/brawer/bern-addresses/issues/315

--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,12 @@ from collections import namedtuple
 import csv
 import os
 from pathlib import Path
+import re
 import urllib.request
 
 
 Page = namedtuple("Page", "id date")
+
 
 def fetch_jpeg(page_id: int) -> Path:
     """
@@ -39,6 +41,61 @@ def fetch_jpeg(page_id: int) -> Path:
             f.write(img)
         os.rename(filepath + ".tmp", filepath)
     return Path(filepath)
+
+
+def parse_pages(pages: str) -> set[int]:
+    """
+    Parse the string passed as the --pages command-line argument.
+
+    Args:
+        pages: A string designating a set of pages, for example
+               "29210355,29210410-29210412".
+
+    Returns:
+        A set of integer page IDs, such as
+        {29210355, 29210410, 29210355, 29210411, 29210355, 29210412}.
+    """
+    result = set()
+    if not pages:
+        return result
+    for s in pages.split(","):
+        s = s.strip()
+        if "-" in s:
+            if m := re.match(r"(\d+)\-(\d+)", s):
+                for page_id in range(int(m.group(1)), int(m.group(2)) + 1):
+                    result.add(page_id)
+            else:
+                raise ValueError(f"not in YYYY-YYYY format: {s}")
+        else:
+            result.add(int(s))
+    return result
+
+
+def parse_years(years: str) -> set[int]:
+    """
+    Parse the string passed as the --years command-line argument.
+
+    Args:
+        years: A string designating a set of years, for example
+               "1880-1883,1905".
+
+    Returns:
+        A set of integer years, such as {1880, 1881, 1882, 1883, 1905}.
+    """
+    result = set()
+    if not years:
+        return result
+    for s in years.split(","):
+        s = s.strip()
+        if "-" in s:
+            if m := re.match(r"(\d{4})\-(\d{4})", s):
+                for year in range(int(m.group(1)), int(m.group(2)) + 1):
+                    result.add(year)
+            else:
+                raise ValueError(f"not in YYYY-YYYY format: {s}")
+        else:
+            result.add(int(s))
+    return result
 
 
 def read_pages():
